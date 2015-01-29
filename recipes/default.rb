6a375fd06cbb0e1fc926969345fa3204a392565f
Richard Lee <dlackty@gmail.com>
2015-01-30 00:28:57 +0800

    Implement dokku-alt installation

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,24 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+apt_package "apt-transport-https"
+
+apt_repository "docker" do
+  uri          "https://get.docker.io/ubuntu"
+  distribution "docker"
+  components   ["main"]
+  keyserver    "keyserver.ubuntu.com"
+  key          "36A1D7869245C8950F966E92D8576A8BA88D21E9"
+end
+
+apt_repository "dokku-alt" do
+  uri          "https://dokku-alt.github.io/dokku-alt"
+  components   ["/"]
+  keyserver    "keys.gnupg.net"
+  key          "EAD883AF"
+end
+
+%w(dokku-alt ruby ruby-sinatra).each do |pkg|
+  apt_package pkg
+end
